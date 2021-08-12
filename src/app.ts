25dd0d2c4b541b8b5e2a0f11e6988df33df7d4b0
jacksonPrimo <jackson.primo12@gmail.com>
2021-08-12 08:57:08 -0300

    finished aggregation activity

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+import { Course } from "./aggregation/Course";
+import { Subject } from "./aggregation/Subject";
+import { Teacher } from "./aggregation/Teacher";
 import { Employee } from "./inheritance/employee";
 import { Enterprise } from "./inheritance/enterprise";
 import { Pf } from "./inheritance/pf";
 import { Pj } from "./inheritance/pj";
+
+console.log("==================== AGREGAÇÃO ===========================");
+
+const teacher1: Teacher  = new Teacher("João Paulo");
+const teacher2: Teacher  = new Teacher("Aislan Rafael");
+const subject1: Subject = new Subject("Programação Orientada a Objetos", 1, teacher1);
+const subject2: Subject = new Subject("Engenharia de Software", 2, teacher2);
+const c1: Course = new Course("Análise e Desenvolvimento de Sistemas");
+c1.AddSubject(subject1);
+c1.AddSubject(subject2);
+c1.showSubjects();
+
+console.log("==================== HERANÇA ===========================");
 
 const enterprite1: Enterprise = new Enterprise("Pague Menos");
 const employee1: Employee = new Pf("Jackson", 1500.00, "66666666666");
