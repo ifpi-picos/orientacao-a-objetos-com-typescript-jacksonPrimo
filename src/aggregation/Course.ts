60ae4f8b5b2ed3882ec9d9c56e64b0e860c3366b
jacksonPrimo <jackson.primo12@gmail.com>
2021-08-12 17:30:00 -0300

    increase getName and getSubjects in course class

--- src/aggregation/Course.ts
+++ src/aggregation/Course.ts
@@ -1,14 +1,19 @@
 import { Subject } from "./Subject";
 
 export class Course {
-  name: string;
-  subjects: Array<Subject>;
+  private name: string;
+  private subjects: Array<Subject>;
   
   constructor(name: string){
     this.name = name;
     this.subjects = [];
   }
-
+  public getName(): string{
+    return this.name
+  }
+  public getSubjects(): Array<Subject>{
+    return this.subjects
+  }
   public AddSubject(s: Subject){
     this.subjects.push(s);
   }
@@ -17,8 +22,8 @@ export class Course {
   }
   public showSubjects(){
     this.subjects.forEach(el => {
-      console.log(`Disciplina: ${el.name} \n`);
-      console.log(`professor: ${el.teacher.name} \n`);
+      console.log(`Disciplina: ${el.getName} \n`);
+      console.log(`professor: ${el.getTeacher().getName()} \n`);
       console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
     });
   }
