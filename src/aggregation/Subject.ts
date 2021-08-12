d26824f3faa3efc42f584d499d1738a754f2913a
jacksonPrimo <jackson.primo12@gmail.com>
2021-08-12 17:30:41 -0300

    added getName and getTeacher in Subject class

--- src/aggregation/Subject.ts
+++ src/aggregation/Subject.ts
@@ -1,12 +1,18 @@
 import { Teacher } from "./Teacher";
 
 export class Subject {
-  name: string;
-  ch: number;
-  teacher: Teacher;
+  private name: string;
+  private ch: number;
+  private teacher: Teacher;
   constructor(name: string, ch: number, teacher: Teacher){
     this.name = name;
     this.ch = ch;
     this.teacher = teacher;
+  }
+  public getName(): string{
+    return this.name
+  }
+  public getTeacher(): Teacher{
+    return this.teacher
   }
 }
